37fa4b004e60303eed327a8593dd09f67a7dac0f
Leo Santillan <lcskode@gmail.com>
2018-11-01 13:32:28 +0800

    Created readme.md to list all things that were done. Added hard coded campgrounds data to and view data on campgrounds.ejs file.

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
-/******************************************************************************
-* Add landing page
-* Add campgrounds page that lists all campgrounds
 
-Each campground has:
-* Name
-* Image
-******************************************************************************/
 
 
 var express = require("express");
@@ -22,10 +15,22 @@ app.get("/", function(req, res){
   res.render("landing");
 });
 
+// list all campgrounds
+app.get("/campgrounds", function(req, res){
+  // dummy data for campgrounds
+  var campgrounds = [
+    {name: "Salmon Creek", image: "https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_1280.jpg"},
+    {name: "Granite Hill", image: "https://cdn.pixabay.com/photo/2016/01/19/16/48/teepee-1149402_1280.jpg"},
+    {name: "Mountain Goat's Rest", image: "https://cdn.pixabay.com/photo/2015/07/10/17/24/night-839807_1280.jpg"}
+  ]
+
+  // pass campgrounds data to list all campgrounds on campgrounds.ejs
+  res.render("campgrounds", {campgrounds: campgrounds});
+});
 
 // ============================================================================
 // START SERVER
 // ============================================================================
 app.listen(3000, function(){
   console.log("Yelpcamp server has started...");
-})
+});
---
+++ readme.md
@@ -0,0 +1,6 @@
+* Add landing page
+* Add campgrounds page that lists all campgrounds
+
+Each campground has:
+* Name
+* Image
